9a0cace2f2dfab62ee6f918e4250a2885f3c7105
KeshavPundhir <pundhirk1@gmail.com>
2026-02-07 23:40:58 +0530

    .

--- Q16.cpp
+++ Q16.cpp
@@ -1,20 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main ()
+int main()
 {
-int a;
-cout<<"enter the number to check : "<< endl;
+    int a;
+    cout << "enter the number to check : " << endl;
 
-cin>>a;
+    cin >> a;
 
+    if (a <= 1)
+    {
+        cout << "not a prime number" << endl;
+    }
 
+    for (int i = 2; i < a; i++)
+    {
 
+        if (a % i == 0)
+        {
+            cout << "not a prime number" << endl;break;
+        }
 
-
-
-
-
-
+        else
+        {
+            cout << "prime number"; break;
+                }
+    }
 
     return 0;
 }
